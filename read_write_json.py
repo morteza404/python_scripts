8fd8e3ebe78a93760009133bc859a0c1f9d8944d
Morteza Shahbazi <mortezash404@gmail.com>
2021-01-23 09:55:32 +0330

    add read_write_json.py

---
+++ read_write_json.py
@@ -0,0 +1,26 @@
+import json
+
+data = {"name" : "morteza", "age" : 29, "city" : "tehran"}
+
+def write_json(data, json_file):
+    """
+    write data to json_file
+    """
+    with open(json_file, "w") as target:
+        json.dump(data, target)
+
+
+def read_json(json_file):
+    """
+    read from json_file
+    """
+    with open(json_file,  "r") as target:
+        json_obj = json.load(target)
+    
+    return json_obj
+
+if __name__ == "__main__":
+
+    # write_json(data, "test.json")
+
+    print(read_json("test.json"))
