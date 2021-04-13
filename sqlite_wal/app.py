0f2abfaee398fe902c0494513fbceefa0914a81f
Morteza Shahbazi <mortezash404@gmail.com>
2021-04-13 15:33:27 +0430

    add sqlite_wal

---
+++ sqlite_wal/app.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+# Open database in autocommit mode by setting isolation_level to None.
+# Set journal mode to WAL.
+def db_connection():
+    conn = sqlite3.connect("test.db", isolation_level=None)
+    conn.execute("pragma journal_mode=wal")
+    return conn
+
+def create():     
+    with db_connection() as db:        
+        try:        
+            cur = db.cursor()
+            cur.execute("""CREATE TABLE IF NOT EXISTS Student (
+                                                  Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                  Name VARCHAR(20),
+                                                  Address VARCHAR(20)
+                                                  )
+                        """)
+            print ("table created successfully")
+        except:
+            print ("error in operation")
+            db.rollback()
+
+def insert(name, address):     
+    with db_connection() as db:        
+        query="INSERT INTO Student (Name, Address) VALUES(?, ?);"
+        try:
+            cur=db.cursor()
+            cur.execute(query, (name, address))
+            db.commit()
+            print ("one record added successfully")
+        except:
+            print ("error in operation")
+            db.rollback()
+
+def read(id):    
+    with db_connection() as db:        
+        query = "SELECT * FROM Student WHERE Id=?;"
+        cur = db.cursor()
+        cur.execute(query, str(id))
+        while True:
+            record=cur.fetchone()
+            if record == None:
+                break
+            print (record)
+
+if __name__ == "__main__":
+    create()
+    insert("Mike", "NY")
+    read(1)
