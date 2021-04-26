ba32435bc9117de5877bca1dfca22dddc69ae689
Morteza Shahbazi <mortezash404@gmail.com>
2021-04-26 09:27:22 +0430

    add dont_care_.py & python_GIL.pdf & true_python_env_shebang.py

---
+++ dont_care_.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.7
+
+# it is better to use /usr/bin/env python3.7 instead of /usr/bin/python3.7
+
+def say_hello():
+	print("hello")
+
+
+# dont care _ 
+for _ in range(10):
+	say_hello()
