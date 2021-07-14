4eab3922df9791dacca50783eec67831ff83cd1b
Morteza Shahbazi <mortezash404@gmail.com>
2021-07-14 09:02:32 +0430

    get_common_sets_elements.py

---
+++ get_common_sets_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+"""
+by & operator we can compare two sets and return common elements.
+"""
+
+set_one = {"ali", "reza", "ahoora", "armin"}
+
+set_two = {"saber", "reza", "mani", "armin"}
+
+print(set_one & set_two)
+
